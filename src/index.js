1ec72dc43525b2096d8c2c0b8a40079eb7643e29
ryntab <57367794+ryntab@users.noreply.github.com>
2022-10-15 16:40:51 -0400

    fix: :bug: skip rejected asset requests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 export default (router, { services, env }) => {
-
   const { AssetsService, MailService } = services;
 
   router.post("/", async (req, res, schema) => {
-
     const mailService = new MailService({
       accountability: req.accountability,
       schema: schema,
@@ -18,28 +16,35 @@ export default (router, { services, env }) => {
 
     let authorityCheck = () => {
       if (env.EMAIL_ALLOW_GUEST_SEND == null || !env.EMAIL_ALLOW_GUEST_SEND) {
-        return (req.accountability.user == null || req.accountability.role == null) ? false : true;
+        return req.accountability.user == null ||
+          req.accountability.role == null
+          ? false
+          : true;
       } else {
         return true;
       }
     };
 
     let attachments = async (fileIDS) => {
-
       let attachments = new Array();
 
       let streamAttachments = new Array();
 
       // Resolve all streams to an array
-      streamAttachments = await Promise.all(
+      streamAttachments = await Promise.allSettled(
         fileIDS.map(function (id) {
           return assetsService.getAsset(id, {});
         })
       );
 
+      // Filter out all rejected promises
+      const resolvedAttachments = streamAttachments.filter(
+        (attachment) => attachment.status === "fulfilled"
+      );
+
       // Create attachment objects from streams
-      streamAttachments.forEach((asset) => {
-        const { stream, file } = asset;
+      resolvedAttachments.forEach((asset) => {
+        const { stream, file } = asset.value;
         let attachment = new Object();
         attachment.contentType = file.type;
         attachment.filename = file.filename_download;
@@ -70,12 +75,12 @@ export default (router, { services, env }) => {
       }
 
       return mail;
-    }
+    };
 
     let send = async (email) => {
       if (authorityCheck()) {
-        await mailService.send(email).then(()=> {
-          res.send('sent');
+        await mailService.send(email).then(() => {
+          res.send("sent");
         });
       } else {
         return res.status(400).send({
